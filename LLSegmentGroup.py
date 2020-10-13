9cafa4b3f77ebc305870f9467d7f04098935f8de
Daniel Wood <s.d.wood.82@googlemail.com>
2020-10-13 20:31:53 +0100

    calculate boundbox of segmentgroup

--- LLSegmentGroup.py
+++ LLSegmentGroup.py
@@ -1,3 +1,7 @@
+from LibLathe.LLBoundBox import BoundBox
+from LibLathe.LLPoint import Point
+
+
 class SegmentGroup:
     '''
     Container Group for segments
@@ -28,3 +32,31 @@ class SegmentGroup:
         Return the number of segments in the segmentgroup
         '''
         return len(self.segments)
+
+    def boundbox(self):
+        '''
+        Return the boundbox for the segmentgroup
+        '''
+        xvalues = []
+        yvalues = []
+        zvalues = []
+
+        # collect all points from each segment by direction
+        for segment in self.get_segments():
+            xvalues.extend(segment.get_all_axis_positions('X'))
+            yvalues.extend(segment.get_all_axis_positions('Y'))
+            zvalues.extend(segment.get_all_axis_positions('Z'))
+
+        XMin = min(xvalues, key=abs)
+        YMin = max(xvalues, key=abs)
+        ZMin = min(yvalues, key=abs)
+        XMax = max(yvalues, key=abs)
+        YMax = min(zvalues, key=abs)
+        ZMax = max(zvalues, key=abs)
+
+        pt1 = Point(XMin, YMin, ZMin)
+        pt2 = Point(XMax, YMax, ZMax)
+
+        segmentGroupBoundBox = BoundBox(pt1, pt2)
+
+        return segmentGroupBoundBox
