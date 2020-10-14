db8d3fb76ee75a1fa0b90c64b2b9449dab16761c
Brett Parker <brettleon123@gmail.com>
2020-10-14 20:54:57 +0100

    sub and add

--- tests/test_Point.py
+++ tests/test_Point.py
@@ -1,5 +1,3 @@
-"""Test for LLPoint.py"""
-
 import os
 import sys
 import unittest
@@ -11,34 +9,45 @@ from LibLathe.LLPoint import Point
 
 
 class test_point(unittest.TestCase):
-    pt = Point(0, 0, 0)
+    """Test for LLPoint.py"""
+
+    pt1 = Point(0, 0, 0)
     pt2 = Point(100, 100, 100)
     pt3 = Point(150, 130, 200)
+    pt3 = Point(200, 200, 200)
 
     def test_distance_to(self):
-        distance = Point.distance_to(self.pt, self.pt2)
+        distance = Point.distance_to(self.pt1, self.pt2)
         self.assertEqual(distance, 173.20508075688772)
 
     def test_angle_to(self):
-        angle = Point.angle_to(self.pt, self.pt2)
+        angle = Point.angle_to(self.pt1, self.pt2)
         self.assertEqual(angle, 45)
 
     def test_nearest(self):
         pts = [self.pt2, self.pt3]
-        nearest = Point.nearest(self.pt, pts)
+        nearest = Point.nearest(self.pt1, pts)
         self.assertEqual(nearest, self.pt2)
 
     def test_is_same_return_false(self):
-        same = Point.is_same(self.pt, self.pt2)
+        same = Point.is_same(self.pt1, self.pt2)
         self.assertEqual(same, False)
 
     def test_is_same_return_true(self):
-        same = Point.is_same(self.pt, self.pt)
+        same = Point.is_same(self.pt1, self.pt1)
         self.assertEqual(same, True)
 
-    def sub(self):
-        sub = Point.sub(selfpt, self.pt2)
-        self.assertEqual(sub, self.pt2)
+    def test_sub(self):
+        sub = Point.sub(self.pt3, self.pt2)
+        self.assertEqual(sub.X, self.pt2.X)
+        self.assertEqual(sub.Y, self.pt2.Y)
+        self.assertEqual(sub.Z, self.pt2.Z)
+
+    def test_add(self):
+        add = Point.add(self.pt2, self.pt2)
+        self.assertEqual(add.X, self.pt3.X)
+        self.assertEqual(add.Y, self.pt3.Y)
+        self.assertEqual(add.Z, self.pt3.Z)
 
 
 if __name__ == '__main__':
