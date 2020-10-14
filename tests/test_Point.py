d91af1dace2971a1b88ecc7c74b24a3d733fa7a2
Brett Parker <brettleon123@gmail.com>
2020-10-14 20:39:25 +0100

    removed duplicate code

--- tests/test_Point.py
+++ tests/test_Point.py
@@ -11,32 +11,34 @@ from LibLathe.LLPoint import Point
 
 
 class test_point(unittest.TestCase):
+    pt = Point(0, 0, 0)
+    pt2 = Point(100, 100, 100)
+    pt3 = Point(150, 130, 200)
 
     def test_distance_to(self):
-        pt = Point(0, 0, 0)
-        pt2 = Point(100, 100, 100)
-        distance = Point.distance_to(pt, pt2)
+        distance = Point.distance_to(self.pt, self.pt2)
         self.assertEqual(distance, 173.20508075688772)
 
     def test_angle_to(self):
-        pt = Point(0, 0, 0)
-        pt2 = Point(100, 100, 100)
-        angle = Point.angle_to(pt, pt2)
+        angle = Point.angle_to(self.pt, self.pt2)
         self.assertEqual(angle, 45)
 
     def test_nearest(self):
-        pt = Point(0, 0, 0)
-        pt2 = Point(100, 100, 100)
-        pt3 = Point(150, 130, 200)
-        pts = [pt2, pt3]
-        nearest = Point.nearest(pt, pts)
-        self.assertEqual(nearest, pt2)
+        pts = [self.pt2, self.pt3]
+        nearest = Point.nearest(self.pt, pts)
+        self.assertEqual(nearest, self.pt2)
 
     def test_is_same_return_false(self):
-        pt = Point(0, 0, 0)
-        pt2 = Point(100, 100, 100)
-        same = Point.is_same(pt, pt2)
+        same = Point.is_same(self.pt, self.pt2)
         self.assertEqual(same, False)
+
+    def test_is_same_return_true(self):
+        same = Point.is_same(self.pt, self.pt)
+        self.assertEqual(same, True)
+
+    def sub(self):
+        sub = Point.sub(selfpt, self.pt2)
+        self.assertEqual(sub, self.pt2)
 
 
 if __name__ == '__main__':
