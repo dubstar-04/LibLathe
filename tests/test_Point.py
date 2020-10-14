6cc9e13d77aee0ccbeba30fa268ecb0a3ad6eba3
Brett Parker <brettleon123@gmail.com>
2020-10-14 20:25:36 +0100

    Add LLPoint test
    Only a shell to begin with but distance, angle and nearest are tested.

---
+++ tests/test_Point.py
@@ -0,0 +1,37 @@
+"""Test for LLPoint.py"""
+
+import os
+import sys
+import unittest
+
+thisFolder = os.path.dirname(os.path.abspath(__file__))
+parentFolder = os.path.dirname(os.path.dirname(thisFolder))
+sys.path.append(parentFolder)
+from LibLathe.LLPoint import Point
+
+
+class test_point(unittest.TestCase):
+
+    def test_distance_to(self):
+        pt = Point(0, 0, 0)
+        pt2 = Point(100, 100, 100)
+        test = Point.distance_to(pt, pt2)
+        self.assertAlmostEqual(test, 173.20508075688772)
+
+    def test_angle_to(self):
+        pt = Point(0, 0, 0)
+        pt2 = Point(100, 100, 100)
+        angle = Point.angle_to(pt, pt2)
+        self.assertAlmostEqual(angle, 45)
+
+    def test_nearest(self):
+        pt = Point(0, 0, 0)
+        pt2 = Point(100, 100, 100)
+        pt3 = Point(150, 130, 200)
+        pts = [pt2, pt3]
+        nearest = Point.nearest(pt, pts)
+        self.assertAlmostEqual(nearest, pt2)
+
+
+if __name__ == '__main__':
+    unittest.main()
