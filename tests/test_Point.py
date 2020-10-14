426d2aecfa4dc84c8a17a37b4e2b19dfc942ffe4
Brett Parker <brettleon123@gmail.com>
2020-10-14 20:31:59 +0100

    Added is_same
    Added is_same and removed almost equal.

--- tests/test_Point.py
+++ tests/test_Point.py
@@ -16,13 +16,13 @@ class test_point(unittest.TestCase):
         pt = Point(0, 0, 0)
         pt2 = Point(100, 100, 100)
         distance = Point.distance_to(pt, pt2)
-        self.assertAlmostEqual(distance, 173.20508075688772)
+        self.assertEqual(distance, 173.20508075688772)
 
     def test_angle_to(self):
         pt = Point(0, 0, 0)
         pt2 = Point(100, 100, 100)
         angle = Point.angle_to(pt, pt2)
-        self.assertAlmostEqual(angle, 45)
+        self.assertEqual(angle, 45)
 
     def test_nearest(self):
         pt = Point(0, 0, 0)
@@ -30,7 +30,13 @@ class test_point(unittest.TestCase):
         pt3 = Point(150, 130, 200)
         pts = [pt2, pt3]
         nearest = Point.nearest(pt, pts)
-        self.assertAlmostEqual(nearest, pt2)
+        self.assertEqual(nearest, pt2)
+
+    def test_is_same_return_false(self):
+        pt = Point(0, 0, 0)
+        pt2 = Point(100, 100, 100)
+        same = Point.is_same(pt, pt2)
+        self.assertEqual(same, False)
 
 
 if __name__ == '__main__':
