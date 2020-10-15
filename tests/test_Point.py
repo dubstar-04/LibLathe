8ce6e992767eadb0e12dc9779e45fcba04402921
Brett Parker <brettleon123@gmail.com>
2020-10-15 17:05:01 +0100

    more tests added
    Added additional tests, specifically around negative numbers.

--- tests/test_Point.py
+++ tests/test_Point.py
@@ -15,6 +15,13 @@ class test_point(unittest.TestCase):
     pt2 = Point(100, 100, 100)
     pt3 = Point(150, 130, 200)
     pt4 = Point(200, 200, 200)
+    pt5 = Point(-100, 0, 100)
+    pt6 = Point(0, 100, 200)
+    pt7 = Point(-200, -200, -200)
+    pt8 = Point(-400, -400, -400)
+    pt9 = Point(-200, -200, -200)
+    pt10 = Point(200, 200, 200)
+
 
     def test_distance_to(self):
         distance = Point.distance_to(self.pt1, self.pt2)
@@ -31,11 +38,11 @@ class test_point(unittest.TestCase):
 
     def test_is_same_return_false(self):
         same = Point.is_same(self.pt1, self.pt2)
-        self.assertEqual(same, False)
+        self.assertFalse(same)
 
     def test_is_same_return_true(self):
         same = Point.is_same(self.pt1, self.pt1)
-        self.assertEqual(same, True)
+        self.assertTrue(same)
 
     def test_sub(self):
         sub = Point.sub(self.pt4, self.pt2)
@@ -43,17 +50,32 @@ class test_point(unittest.TestCase):
         self.assertEqual(sub.Y, self.pt2.Y)
         self.assertEqual(sub.Z, self.pt2.Z)
 
+        subNegative = Point.add(self.pt7, self.pt7)
+        self.assertEqual(subNegative.X, self.pt8.X)
+        self.assertEqual(subNegative.Y, self.pt8.Y)
+        self.assertEqual(subNegative.Z, self.pt8.Z)
+
     def test_add(self):
         add = Point.add(self.pt2, self.pt2)
         self.assertEqual(add.X, self.pt4.X)
         self.assertEqual(add.Y, self.pt4.Y)
         self.assertEqual(add.Z, self.pt4.Z)
 
+        addNegative = Point.add(self.pt5, self.pt2)
+        self.assertEqual(addNegative.X, self.pt6.X)
+        self.assertEqual(addNegative.Y, self.pt6.Y)
+        self.assertEqual(addNegative.Z, self.pt6.Z)
+
     def test_multiply(self):
         multiply = Point.multiply(self.pt2, 0)
         self.assertEqual(multiply.X, self.pt1.X)
         self.assertEqual(multiply.Y, self.pt1.Y)
         self.assertEqual(multiply.Z, self.pt1.Z)
+
+        multiplyNegative = Point.multiply(self.pt7, 1)
+        self.assertEqual(multiplyNegative.X, self.pt7.X)
+        self.assertEqual(multiplyNegative.Y, self.pt7.Y)
+        self.assertEqual(multiplyNegative.Z, self.pt7.Z)
 
     def test_rotate(self):
         rotate = Point.rotate(self.pt2, 90)
