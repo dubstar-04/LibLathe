a0b93e2e63eec9d9c472cd1ddfefea11f6680e46
Brett Parker <brettleon123@gmail.com>
2020-10-15 19:36:08 +0100

    file read

---
+++ imagedraw/gcodeDraw.py
@@ -0,0 +1,41 @@
+inputFile = 'profile.gcode'
+
+file = open(inputFile, 'r')
+code = []
+
+for line in file:
+    line = line.lstrip().rstrip('\n ')
+
+    if line == '':
+        continue
+
+    if line[0] == 'G':
+        col = {}
+
+        commands = line.split(' ')
+
+        for i in commands[1:]:
+            if i[0] == 'X':
+                col['x'] = float(i[1:])
+
+            elif i[0] == 'Y':
+                col['y'] = float(i[1:])
+
+            elif i[0] == 'Z':
+                col['z'] = float(i[1:])
+
+            elif i[0] == 'F':
+                # can do something with speeds here in future
+                continue
+
+            else:
+                print(line)
+                raise Warning('Unknown character!')
+
+    code.append(col)
+
+if isinstance(inputFile, str):
+    file.close()
+
+# uncomment below to see current output
+# print(code)
