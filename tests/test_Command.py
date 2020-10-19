1ac61ddcb97dbdf00f1de7c79a06599c8ee3f040
Brett Parker <brettleon123@gmail.com>
2020-10-19 20:19:46 +0100

    Added LLCommand test (#66)
    * Added LLCommand tests
    
    Basic but all are good
    
    * Expanded tests
    
    * Flake8 fixes

---
+++ tests/test_Command.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import unittest
+
+thisFolder = os.path.dirname(os.path.abspath(__file__))
+parentFolder = os.path.dirname(os.path.dirname(thisFolder))
+sys.path.append(parentFolder)
+from LibLathe.LLCommand import Command
+
+
+class test_command(unittest.TestCase):
+    """Test for LLBoundBox.py"""
+    def setUp(self):
+        self.params = {'X': -20, 'Y': 0, 'Z': 6, 'F': 10}
+
+    def test_get_movement(self):
+        command = Command('G0')
+        movement = command.get_movement()
+        self.assertEqual(movement, 'G0')
+
+    def test_params(self):
+        command = Command('G0', self.params)
+        params = command.get_params()
+        self.assertEqual(params, self.params)
+
+    def test_to_string(self):
+        command = Command('G0')
+        string = command.to_string()
+        self.assertEqual(string, 'G0')
+
+        command = Command('G0', self.params)
+        string = command.to_string()
+        self.assertEqual(string, 'G0 X-20 Y0 Z6 F10')
+
+
+if __name__ == '__main__':
+    unittest.main()
