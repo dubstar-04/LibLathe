c60e35728579488a155077a1daac05a1692f9719
Brett Parker <brettleon123@gmail.com>
2020-10-20 19:57:07 +0100

    Added get_params test

--- tests/test_BaseOP.py
+++ tests/test_BaseOP.py
@@ -13,17 +13,25 @@ class test_BaseOP(unittest.TestCase):
 
     def setUp(self):
         self.baseop = BaseOP()
-        self.props = {'min_dia': 0, 'extra_dia': 0, 'start_offset': 0, 'end_offset': 0, 'allow_grooving': False,
+        self.setProps = {'min_dia': 0, 'extra_dia': 0, 'start_offset': 0, 'end_offset': 0, 'allow_grooving': False,
                       'allow_facing': False, 'allow_roughing': True, 'allow_finishing': True, 'step_over': 1,
                       'finish_passes': 2, 'hfeed': 10, 'vfeed': 10}
+        self.getProps = {'min_dia': 0, 'extra_dia': 0, 'start_offset': 0, 'end_offset': 0, 'allow_grooving': False,
+                      'allow_facing': False, 'allow_roughing': True, 'allow_finishing': True, 'step_over': 1.5,
+                      'finish_passes': 2, 'hfeed': 100, 'vfeed': 50}
 
     def test_set_params(self):
-        self.baseop.set_params(self.props)
+        self.baseop.set_params(self.setProps)
         self.assertEqual(self.baseop.step_over, 1)
         self.assertEqual(self.baseop.hfeed, 10)
         self.assertEqual(self.baseop.vfeed, 10)
         self.assertTrue(self.baseop.allow_roughing)
         self.assertFalse(self.baseop.allow_facing)
+
+    def test_get_params(self):
+        params = self.baseop.get_params()
+        for i in params:
+            self.assertEqual(params[i], self.getProps[i])
 
 
 if __name__ == '__main__':
