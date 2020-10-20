ac89e41d913190085be4045c76b42b73eb778709
Brett Parker <brettleon123@gmail.com>
2020-10-20 19:50:23 +0100

    Updated SegmentGroup test (#75)
    * Updated segmentgroup test
    
    * Flake8 issue

--- tests/test_SegmentGroup.py
+++ tests/test_SegmentGroup.py
@@ -33,11 +33,9 @@ class test_segment_group(unittest.TestCase):
 
         self.inverseLineSegment1 = Segment(self.pt3, self.pt4)
 
-        self.arcSegment1 = Segment(self.pt1, self.pt2, 1.5)
-        self.arcSegment2 = Segment(self.pt7, self.pt8, 1.5)
-
-        self.inverseArcSegment1 = Segment(self.pt1, self.pt2, -1.5)
-        self.inverseArcSegment2 = Segment(self.pt7, self.pt8, -1.5)
+        self.hfeed = 100
+        self.vfeed = 50
+        self.step_over = 1.5
 
     def test_add_segment(self):
         self.segmentGroup.add_segment(self.lineSegment1)
@@ -88,6 +86,75 @@ class test_segment_group(unittest.TestCase):
         self.assertEqual(YMax, max(self.pt1.Y, self.pt2.Y))
         ZMax = boundbox.ZMax
         self.assertEqual(ZMax, max(self.pt1.Z, self.pt2.Z))
+
+    def test_join_segments(self):
+        self.segmentGroup.add_segment(self.lineSegment1)
+        self.segmentGroup.join_segments()
+        start = self.segmentGroup.segments[0].start
+        end = self.segmentGroup.segments[0].end
+        self.assertEqual(start, self.pt1)
+        self.assertEqual(end, self.pt2)
+
+    def test_previous_segment_connected_false(self):
+        self.segmentGroup.add_segment(self.lineSegment1)
+        self.segmentGroup.add_segment(self.lineSegment2)
+        self.assertFalse(self.segmentGroup.previous_segment_connected(self.lineSegment2))
+
+    def test_previous_segment_connected_true(self):
+        self.segmentGroup.add_segment(self.lineSegment1)
+        self.segmentGroup.add_segment(self.inverseLineSegment1)
+        self.assertFalse(self.segmentGroup.previous_segment_connected(self.inverseLineSegment1))
+
+    def test_get_min_retract_x(self):
+        self.segmentGroup.add_segment(self.lineSegment1)
+        min_x = self.segmentGroup.get_min_retract_x(self.lineSegment1, self.segmentGroup)
+        self.assertEqual(min_x, 100)
+
+        self.segmentGroup.add_segment(self.lineSegment3)
+        min_x = self.segmentGroup.get_min_retract_x(self.lineSegment3, self.segmentGroup)
+        self.assertEqual(min_x,  -164.74)
+
+    def test_to_commands_size(self):
+        self.segmentGroup.add_segment(self.lineSegment1)
+        boundbox = self.segmentGroup.boundbox()
+        cmds = self.segmentGroup.to_commands(self.segmentGroup, boundbox, self.step_over, self.hfeed, self.vfeed)
+        self.assertEqual(len(cmds), 7)
+
+    def test_to_commands_movement(self):
+        self.segmentGroup.add_segment(self.lineSegment1)
+        boundbox = self.segmentGroup.boundbox()
+        cmds = self.segmentGroup.to_commands(self.segmentGroup, boundbox, self.step_over, self.hfeed, self.vfeed)
+        # Expected return
+        # Command Movements
+        # ['G18', 'G0', 'G0', 'G0', 'G1', 'G0', 'G0']
+        self.assertEqual(cmds[0].Movement, 'G18')
+        self.assertEqual(cmds[1].Movement, 'G0')
+        self.assertEqual(cmds[2].Movement, 'G0')
+        self.assertEqual(cmds[3].Movement, 'G0')
+        self.assertEqual(cmds[4].Movement, 'G1')
+        self.assertEqual(cmds[5].Movement, 'G0')
+        self.assertEqual(cmds[6].Movement, 'G0')
+
+    def test_to_commands_params(self):
+        self.segmentGroup.add_segment(self.lineSegment1)
+        boundbox = self.segmentGroup.boundbox()
+        cmds = self.segmentGroup.to_commands(self.segmentGroup, boundbox, self.step_over, self.hfeed, self.vfeed)
+        # Expected return
+        # Command Params
+        # {}
+        # {'X': 0, 'Y': 0, 'Z': 101.5, 'F': 100}
+        # {'X': 0, 'Y': 0, 'Z': 0, 'F': 100}
+        # {'X': 0, 'Y': 0, 'Z': 0, 'F': 100}
+        # {'X': 100, 'Y': 0, 'Z': 100, 'F': 100}
+        # {'X': 98.5, 'Y': 0, 'Z': 100, 'F': 100}
+        # {'X': 98.5, 'Y': 0, 'Z': 101.5, 'F': 100}
+        self.assertEqual(cmds[0].Params, {})
+        self.assertEqual(cmds[1].Params, {'X': 0, 'Y': 0, 'Z': 101.5, 'F': 100})
+        self.assertEqual(cmds[2].Params, {'X': 0, 'Y': 0, 'Z': 0, 'F': 100})
+        self.assertEqual(cmds[3].Params, {'X': 0, 'Y': 0, 'Z': 0, 'F': 100})
+        self.assertEqual(cmds[4].Params, {'X': 100, 'Y': 0, 'Z': 100, 'F': 100})
+        self.assertEqual(cmds[5].Params, {'X': 98.5, 'Y': 0, 'Z': 100, 'F': 100})
+        self.assertEqual(cmds[6].Params, {'X': 98.5, 'Y': 0, 'Z': 101.5, 'F': 100})
 
 
 if __name__ == '__main__':
