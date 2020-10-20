0f707f295247b851b264a6ab5e5e894980f1191f
Brett Parker <brettleon123@gmail.com>
2020-10-20 19:22:13 +0100

    Flake8 issues

--- tests/test_BaseOP.py
+++ tests/test_BaseOP.py
@@ -10,11 +10,12 @@ from LibLathe.LLBaseOP import BaseOP
 
 class test_BaseOP(unittest.TestCase):
     """Test for LLBaseOP.py"""
+
     def setUp(self):
         self.baseop = BaseOP()
         self.props = {'min_dia': 0, 'extra_dia': 0, 'start_offset': 0, 'end_offset': 0, 'allow_grooving': False,
-                 'allow_facing': False, 'allow_roughing': True, 'allow_finishing': True, 'step_over': 1,
-                 'finish_passes': 2, 'hfeed': 10, 'vfeed': 10}
+                      'allow_facing': False, 'allow_roughing': True, 'allow_finishing': True, 'step_over': 1,
+                      'finish_passes': 2, 'hfeed': 10, 'vfeed': 10}
 
     def test_set_params(self):
         self.baseop.set_params(self.props)
