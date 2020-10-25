9f55f1bd4bf94c6dc145f3e0ae24ce5a5ad01747
Daniel Wood <s.d.wood.82@googlemail.com>
2020-10-25 22:07:03 +0000

    Create test_Tool.py
    create a test shell for tool.py

---
+++ tests/test_Tool.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import unittest
+
+thisFolder = os.path.dirname(os.path.abspath(__file__))
+parentFolder = os.path.dirname(thisFolder)
+sys.path.append(parentFolder)
+from liblathe.tool import Tool
+from liblathe.tool import ToolOri
+
+
+class test_tool(unittest.TestCase):
+    """Test for tool.py"""
+    def setUp(self):
+        self.tool = Tool('DCMT070204R')
+
+    def test_create_tool(self):
+        shape = self.tool.shape
+        length = self.tool.length
+        nose_radius = self.tool.nose_radius
+        direction = self.tool.direction
+        orientation = self.tool.orientation
+
+        print(shape, length, nose_radius, direction, orientation)
+        self.assertEqual(shape, "D")
+        self.assertEqual(length, "07")
+        self.assertEqual(nose_radius, "04")
+        self.assertEqual(direction, "R")
+        self.assertEqual(orientation, ToolOri.X)
+
+    def test_getShapeAngle(self):
+        shapeAngle = self.tool.getShapeAngle()
+        self.assertEqual(shapeAngle, 55)
+
+    def test_getEdgeLength(self):
+        edgeLength = self.tool.getEdgeLength()
+        self.assertEqual(edgeLength, 6.35)
+
+
+if __name__ == '__main__':
+    unittest.main()
