91df374ffe2b69dd31f5320b5fb4844fbdab0bc1
Daniel Wood <s.d.wood.82@googlemail.com>
2020-10-29 19:56:38 +0000

    Create parting operation shell

---
+++ liblathe/part_op.py
@@ -0,0 +1,14 @@
+import liblathe.base_op
+
+
+class PartOP(liblathe.base_op.BaseOP):
+
+    def generate_path(self):
+        """Generate the path for the Part operation"""
+        pass
+
+    def generate_gcode(self):
+        """Generate Gcode for the op segments"""
+
+        Path = []
+        return Path
