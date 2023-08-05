0fb67d5ae482e33d107b4fe6e924924682286dfa
Daniel Wood <s.d.wood.82@googlemail.com>
2023-08-05 11:16:47 +0100

    Add lead in to the profile op

--- liblathe/profile_op.py
+++ liblathe/profile_op.py
@@ -1,23 +1,46 @@
 import liblathe.base_op
+from liblathe.point import Point
+from liblathe.segment import Segment
+from liblathe.segmentgroup import SegmentGroup
 
 
 class ProfileOP(liblathe.base_op.BaseOP):
 
     def generate_path(self):
         """Generate the path for the profile operation"""
-
+        # get the defeature part profile
         profile_segment_group = self.part_segment_group.defeature(self.stock.z_min, self.tool, self.allow_grooving)
+        # define the base segment group using the stock to leave as a datum
         base_segment_group = profile_segment_group.offset_path(self.stock_to_leave)
-        self.tool_paths.append(base_segment_group)
-        f_pass = 1
+        f_pass = 0
         while f_pass < self.finish_passes:
-            segmentgroup = base_segment_group.offset_path(self.step_over * f_pass)
-            
-            if segmentgroup.intersects_group(self.part_segment_group):
+            # create a new segment group
+            segmentgroup = SegmentGroup()
+            # generate the offset profile path
+            segmentgroup.extend(base_segment_group.offset_path(self.step_over * f_pass))
+            # check if the generated pass intersects the part. use a small offset to reduce false positives
+            if segmentgroup.intersects_group(self.part_segment_group.offset_path(-0.001)):
                 raise ValueError("Calculated profile path intersects part")
-            
+            # add lead in to the profile path
+            self.add_leadin(segmentgroup)
+            # add the segment group to the tool paths
             self.tool_paths.append(segmentgroup)
+            # increment f_pass to the next pass
             f_pass += 1
+
+    def add_leadin(self, segmentgroup):
+        # get the first segment of the segment group
+        segment = segmentgroup.get_segments()[0]
+        # calculate the new z_pos with lead in
+        z_pos = segment.start.Z + self.clearance
+        # create the new start point
+        start_point = Point(segment.start.X, segment.start.Y, z_pos)
+        # use the segments start as the leadin end point
+        end_point = segment.start
+        # create the leadin segment
+        leadin = Segment(start_point, end_point)
+        # add the leadin segment at the start of segment group
+        segmentgroup.insert_segment(leadin, 0)
 
     def generate_gcode(self):
         """Generate Gcode for the op segments"""
