8da3f62fe2a4b38219f30a7b391d20f4fa144a47
Daniel Wood <s.d.wood.82@googlemail.com>
2024-12-17 18:32:30 +0000

    Added arc test

--- tests/base/test_segment.py
+++ tests/base/test_segment.py
@@ -317,9 +317,14 @@ class test_segment(unittest.TestCase):
         self.assertTrue(on_seg)
 
     def test_distance_to_point(self):
+
         point = Point(10.73243, -9.97400)
         dist = self.lineSegment1.distance_to_point(point)
         self.assertAlmostEqual(dist, 4.24536, 4)
+
+        # TODO: remove this test when the arc support is removed from segement
+        dist = self.arcSegment1.distance_to_point(point)
+        self.assertAlmostEqual(dist, 4.65147, 4)
 
 
 if __name__ == '__main__':
